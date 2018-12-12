c79b0fe1320710b848b556d7f55849e98e51f54d
ignacio <nachogarces15@gmail.com>
2018-12-12 03:27:52 -0300

    complete task

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,24 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
   def destroy
     reset_session
     redirect_to root_path
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user.nil?
+      redirect_to users_sign_in_path
+      return
+    end
+    if @user.password == params[:user][:password]
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to users_sign_up_path
+    end
   end
 end
